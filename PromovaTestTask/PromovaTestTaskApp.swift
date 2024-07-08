0ce39d07e987544554492ee22fe4e50c122e0faf
Bohdan Pokhidnia <bogdanpoh@icloud.com>
2024-07-08 18:28:22 +0300

    Initial Commit

---
+++ PromovaTestTask/PromovaTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromovaTestTaskApp.swift
+//  PromovaTestTask
+//
+//  Created by Bohdan Pokhidnia on 08.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct PromovaTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
