66b0732a5ee1c07e2402e8eb724f033f3e4ea843
Bohdan Pokhidnia <bogdanpoh@icloud.com>
2024-07-10 21:55:49 +0300

    sorted cases like alphabet

--- PromovaTestTask/Modules/Categories/CategoriesList.swift
+++ PromovaTestTask/Modules/Categories/CategoriesList.swift
@@ -19,14 +19,13 @@ struct CategoriesList {
     }
     
     enum Action {
+        case alert(PresentationAction<Alert>)
+        case categoriesResponse(categories: [Category])
+        case categoryViewTapped(category: Category)
+        case network(error: NetworkError)
         case onAppear
         case order(categories: [Category])
-        case categoriesResponse(categories: [Category])
-        case network(error: NetworkError)
-        case categoryViewTapped(category: Category)
-        case checkStatus(category: Category)
         case path(StackActionOf<Path>)
-        case alert(PresentationAction<Alert>)
         case timerFire(category: Category)
         
         enum Alert {
@@ -49,6 +48,22 @@ struct CategoriesList {
     var body: some ReducerOf<Self> {
         Reduce { (state, action) in
             switch action {
+            case let .alert(.presented(.showAd(id))):
+                guard let category = state.categories[id: id] else {
+                    return .none
+                }
+                state.isLoading = true
+                
+                return .run { (send) in
+                    for await _ in clock.timer(interval: .seconds(2)) {
+                        await send(.timerFire(category: category))
+                    }
+                }
+                .cancellable(id: CancelID.timer)
+                
+            case .alert:
+                return .none
+                
             case .onAppear:
                 guard state.categories.isEmpty else {
                     return .none
@@ -67,24 +82,12 @@ struct CategoriesList {
                     }
                 }
                 
-            case let .order(categories):
-                let orderedCategories = categories.sorted(by: { $0.order < $1.order })
-                return .send(.categoriesResponse(categories: orderedCategories))
-                
             case let .categoriesResponse(categories):
                 state.isLoading = false
                 state.categories = IdentifiedArray(uniqueElements: categories)
                 return .none
                 
-            case let .network(error):
-                state.isLoading = false
-                print("[dev] \(error)")
-                return .none
-                
             case let .categoryViewTapped(category):
-                return .send(.checkStatus(category: category))
-                
-            case let .checkStatus(category):
                 switch category.status {
                 case .free:
                     state.path.append(.fact(FactFeature.State(category: category)))
@@ -97,29 +100,22 @@ struct CategoriesList {
                 }
                 return .none
                 
-            case .path:
+            case let .network(error):
+                state.isLoading = false
+                print("[dev] \(error)")
                 return .none
                 
-            case let .alert(.presented(.showAd(id))):
-                guard let category = state.categories[id: id] else {
-                    return .none
-                }
-                state.isLoading = true
+            case let .order(categories):
+                let orderedCategories = categories.sorted(by: { $0.order < $1.order })
+                return .send(.categoriesResponse(categories: orderedCategories))
                 
-                return .run { (send) in
-                    for await _ in clock.timer(interval: .seconds(2)) {
-                        await send(.timerFire(category: category))
-                    }
-                }
-                .cancellable(id: CancelID.timer)
+            case .path:
+                return .none
                 
             case let .timerFire(category):
                 state.isLoading = false
                 state.path.append(.fact(FactFeature.State(category: category)))
                 return .cancel(id: CancelID.timer)
-                
-            case .alert:
-                return .none
             }
         }
         .ifLet(\.$alert, action: \.alert)
